f648723bd53c3333e98439dfb937e88776ee88b5
Ninj0r <james.j.hofbauer@gmail.com>
2017-03-03 23:21:12 +0900

    Version without null

--- src/App.java
+++ src/App.java
@@ -9,29 +9,24 @@ public class App {
     public static void main(String[] args) {
         Integer[] array = new Integer[]{1,2,3,4,5,5,5,6,7,7,7,9,9};
         List<Integer> modeInts = new ArrayList<>();
-        Integer trackedInt = null;
-        int currCount = 0;
-        int maxCount = 0;
-        for (int i = 0; i < array.length; i++) {
+        int trackedInt = array[0];
+        int currCount = 1;
+        int maxCount = 1;
+        for (int i = 1; i < array.length; i++) {
             int currInt = array[i];
-            if (null == trackedInt) {
-                trackedInt = currInt;
-                currCount = 1;
+            if (currInt == trackedInt) {
+                currCount++;
             } else {
-                if (currInt == trackedInt) {
-                    currCount++;
-                } else {
-                    if (currCount > maxCount) {
-                        maxCount = currCount;
-                        modeInts.clear();
-                        modeInts.add(trackedInt);
-                    } else if (currCount == maxCount) {
-                        modeInts.add(trackedInt);
-                    }
-
-                    currCount = 1;
-                    trackedInt = currInt;
+                if (currCount > maxCount) {
+                    maxCount = currCount;
+                    modeInts.clear();
+                    modeInts.add(trackedInt);
+                } else if (currCount == maxCount) {
+                    modeInts.add(trackedInt);
                 }
+
+                currCount = 1;
+                trackedInt = currInt;
             }
         }
 
@@ -40,4 +35,3 @@ public class App {
         }
     }
 }
-
